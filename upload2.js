787f227b6247184b9dcca5f7778b9e943405d34c
Mustak Ahmed <mahmed0715@gmail.com>
2024-02-15 21:09:00 +0600

    fix upload 2

--- upload2.js
+++ upload2.js
@@ -28,6 +28,6 @@ app.post('/upload2', upload.single('file'), (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log('Server running on port 3000');
 });
