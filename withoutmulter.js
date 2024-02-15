e857c82796fee05322b3a6913ec4ff19ab2f9f0d
Mustak Ahmed <mahmed0715@gmail.com>
2024-02-15 19:11:37 +0600

    check if reaching

--- withoutmulter.js
+++ withoutmulter.js
@@ -29,6 +29,7 @@ app.get('/', (req, res) => {
 });
 
 // Handle the file upload
+app.use((req, res, next)=>{console.log({body: req.body}); next()})
 app.post('/upload', upload.any(), (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
