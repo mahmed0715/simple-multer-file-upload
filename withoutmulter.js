77b20c0f18f994e20dd8ee0e34abc5dcb7cfb2c5
Mustak Ahmed <mahmed0715@gmail.com>
2024-02-15 17:58:16 +0600

    express upload

--- withoutmulter.js
+++ withoutmulter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Enable file upload middleware
 app.use(fileUpload());
